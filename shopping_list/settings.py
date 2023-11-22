efa443ade6705bb560ac862dfec200ea9ace50a3
Kevin I <kevin.ignatius@ui.ac.id>
2023-11-22 14:13:48 +0700

    +building flutter and tamat\

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -129,6 +129,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ["https://kevin-ignatius-tutorial.pbp.cs.ui.ac.id/"],
 
 
 # Static files (CSS, JavaScript, Images)
